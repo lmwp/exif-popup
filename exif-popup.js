3d0c2ecbe11fcd99f87339e4ada71ae12cf344a3
Terri Dev <lm-wp@csnweb.fr>
2023-05-10 16:01:08 +0200

    premiere version

--- exif-popup.js
+++ exif-popup.js
@@ -1,84 +1,58 @@
-const exifCache = {};
-const allElements = document.getElementsByTagName('*');
+jQuery(document).ready(function($) {
+  const exifCache = {};
 
-for (let i = 0; i < allElements.length; i++) {
-  const element = allElements[i];
+  $('img').on('mouseenter', function(e) {
+      const img = $(this);
+      const attachmentId = img.attr('data-attachment-id');
 
-  // Check if element has a background image
-  const backgroundImage = window.getComputedStyle(element).getPropertyValue('background-image');
-  if (backgroundImage && backgroundImage !== 'none') {
-    const imageUrl = backgroundImage.match(/url\(['"]?([^'"\(\)]+)['"]?\)/i)[1];
-    if (/\.(jpe?g|png|gif)$/i.test(imageUrl)) {
-      addExifPopupToElement(element, imageUrl);
-    }
-  }
-
-  // Check if element is an image
-  if (element.tagName.toLowerCase() === 'img') {
-    const imageUrl = element.getAttribute('src');
-    if (/\.(jpe?g|png|gif)$/i.test(imageUrl)) {
-      addExifPopupToElement(element, imageUrl);
-    }
-  }
-}
-
-function addExifPopupToElement(element, imageUrl) {
-  const attachmentId = getImageAttachmentId(imageUrl);
-  if (attachmentId) {
-    if (exifCache[attachmentId]) {
-      chatgptDisplayExifPopup({
-        pageX: element.getBoundingClientRect().right,
-        pageY: element.getBoundingClientRect().bottom,
-      }, exifCache[attachmentId]);
-    } else {
-      const ajaxUrl = exifPopupAjax.ajaxurl;
-      const data = {
-        action: 'exif_popup',
-        attachment_id: attachmentId
-      };
-      const onSuccess = function (response) {
-        if (response.success) {
-          const exifData = response.data;
-          exifCache[attachmentId] = exifData;
-          chatgptDisplayExifPopup({
-            pageX: element.getBoundingClientRect().right,
-            pageY: element.getBoundingClientRect().bottom,
-          }, exifData);
-        }
-      };
-      $.post(ajaxUrl, data, onSuccess);
-    }
-  }
-}
-
-function getImageAttachmentId(imageUrl) {
-  const match = imageUrl.match(/wp-image-(\d+)/);
-  if (match && match[1]) {
-    return match[1];
-  }
-  return null;
-}
-
-function chatgptDisplayExifPopup(e, exifData) {
-  const popup = $('<div class="exif-popup"></div>');
-  popup.css({
-    position: 'absolute',
-    left: e.pageX + 5,
-    top: e.pageY + 5,
-    background: 'white',
-    border: '1px solid #ccc',
-    padding: '5px',
-    zIndex: 10000,
-    //display: 'none'
+      if (attachmentId) {
+          if (exifCache[attachmentId]) {
+              chatgptDisplayExifPopup(e, exifCache[attachmentId]);
+          } else {
+              $.ajax({
+                  url: exifPopupAjax.ajaxurl,
+                  method: 'POST',
+                  data: {
+                      action: 'exif_popup',
+                      attachment_id: attachmentId
+                  },
+                  success: function(response) {
+                      if (response.success) {
+                          const exifData = response.data;
+                          exifCache[attachmentId] = exifData;
+                          chatgptDisplayExifPopup(e, exifData);
+                      }
+                  }
+              });
+          }
+      }
   });
 
-  popup.html(
-    `<strong>${exifPopup.l10n.title}:</strong> ${exifData.title}<br>
-    <strong>${exifPopup.l10n.width}:</strong> ${exifData.width}<br>
-    <strong>${exifPopup.l10n.height}:</strong> ${exifData.height}<br>
-    <strong>${exifPopup.l10n.file_size}:</strong> ${exifData.file_size}`
-  );
+  function chatgptDisplayExifPopup(e, exifData) {
+      const popup = $('<div class="exif-popup"></div>');
+      popup.css({
+          position: 'absolute',
+          left: e.pageX + 5,
+          top: e.pageY + 5,
+          background: 'white',
+          border: '1px solid #ccc',
+          padding: '5px',
+          zIndex: 10000,
+          display: 'none'
+      });
 
-  $('body').append(popup);
-  popup.fadeIn(200);
-}
+      popup.html(
+          `Title: ${exifData.title}<br>
+          Width: ${exifData.width}<br>
+          Height: ${exifData.height}<br>
+          File Size: ${exifData.file_size}`
+      );
+
+      $('body').append(popup);
+      popup.fadeIn(200);
+  }
+
+  $('img').on('mouseleave', function() {
+      $('.exif-popup').remove();
+  });
+});
--- exit-popup.php
+++ exit-popup.php
@@ -9,7 +9,6 @@ Author URI: https://example.com/
 Text domain = exif-popup
 License: GPL2
 */
-
 // Enqueue scripts
 add_action('wp_enqueue_scripts', 'chatgpt_exif_popup_enqueue_scripts');
 function chatgpt_exif_popup_enqueue_scripts() {
@@ -42,55 +41,3 @@ add_action('wp_enqueue_scripts', 'chatgpt_exif_popup_enqueue_styles');
 function chatgpt_exif_popup_enqueue_styles() {
     wp_enqueue_style('exif-popup', plugin_dir_url(__FILE__) . 'exif-popup.css', array(), '1.0');
 }
-// Add plugin options page
-add_action('admin_menu', 'chatgpt_exif_popup_options_page');
-function chatgpt_exif_popup_options_page() {
-    add_menu_page(
-        'EXIF Popup Options', // Page title
-        'EXIF Popup', // Menu title
-        'manage_options', // Capability required to access the page
-        'exif-popup-options', // Menu slug
-        'chatgpt_exif_popup_options_callback', // Callback function to render the page
-        'dashicons-format-image', // Icon URL
-        100 // Position in the menu
-    );
-}
-
-// Render options page
-function chatgpt_exif_popup_options_callback() {
-    // Check if user has permission to access the page
-    if (!current_user_can('manage_options')) {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
-    }
-
-    // Get current option value
-    $enabled = get_option('chatgpt_exif_popup_enabled');
-
-    // Handle form submission
-    if (isset($_POST['chatgpt_exif_popup_submit'])) {
-        $enabled = isset($_POST['chatgpt_exif_popup_enabled']) ? 1 : 0;
-        update_option('chatgpt_exif_popup_enabled', $enabled);
-        echo '<div class="updated"><p>Options saved.</p></div>';
-    }
-
-    // Render options form
-    ?>
-    <div class="wrap">
-        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-        <form method="post" action="">
-            <table class="form-table">
-                <tbody>
-                    <tr>
-                        <th scope="row">Enable EXIF Popup</th>
-                        <td><label><input type="checkbox" name="chatgpt_exif_popup_enabled" value="1" <?php checked(1, $enabled); ?>> Enabled</label></td>
-                    </tr>
-                </tbody>
-            </table>
-            <p class="submit"><input type="submit" name="chatgpt_exif_popup_submit" class="button-primary" value="Save Changes"></p>
-        </form>
-    </div>
-    <?php
-}
-
-// Initialize default option value
-add_option('chatgpt_exif_popup_enabled', 1);
